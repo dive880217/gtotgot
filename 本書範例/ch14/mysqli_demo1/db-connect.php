6147e6ace4187a2eeb8b3dea57b2182474f1f534
黃昱銘 <dive880217@gmail>
2022-07-11 06:40:03 +0800

    3r

---
+++ 本書範例/ch14/mysqli_demo1/db-connect.php
@@ -0,0 +1,22 @@
+<?php
+	//資料庫主機設定
+ $serverName="localhost";
+ $username="admin";
+ $password="12345";
+ $dbname="hand_db";
+
+try{
+    $db_host=new PDO("mysql:host={$serverName};dbname=
+	{$dbname};charset=utf8",$username,$password);
+    $db_host->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //讓資料庫顯示錯誤原因
+}catch(PDOException $e){
+    echo "資料庫連線失敗";
+    echo "Error: ".$e->getMessage();
+    exit;
+}
+
+?>
+
+
+
+
